34b2b249f124be0120f198ed7725404f6e2c7910
DRAKOS <drakostius2@gmail.com>
2020-11-04 00:12:41 +0200

    Updates

---
+++ precache-manifest.5328202ed5f94e3ec8e21e56c23157a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92cc767bbc6a5852bd7316a45bc5e3c",
+    "url": "/goit-react-hw-02-phonebook.git/index.html"
+  },
+  {
+    "revision": "00c68dd50961d6edd688",
+    "url": "/goit-react-hw-02-phonebook.git/static/css/main.917ff00f.chunk.css"
+  },
+  {
+    "revision": "75fff3a0b2d56c9ea00f",
+    "url": "/goit-react-hw-02-phonebook.git/static/js/2.dc2cc7bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook.git/static/js/2.dc2cc7bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c68dd50961d6edd688",
+    "url": "/goit-react-hw-02-phonebook.git/static/js/main.811afba7.chunk.js"
+  },
+  {
+    "revision": "54ce14907dddbe292d3b",
+    "url": "/goit-react-hw-02-phonebook.git/static/js/runtime-main.5aebe2a9.js"
+  }
+]);
